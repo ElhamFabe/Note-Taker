4f8ce49ffabb90f0f1e7078065683582064c61b1
Elham Fabe <elhamfabe@gmail.com>
2020-09-10 15:31:41 -0400

    api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,12 @@
 // ===============================================================================
 // LOAD DATA
 // We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
 
-var tableData = require("../data/tableData");
-var waitListData = require("../data/waitinglistData");
+
+const express = require('express');
+const path = require ('path');
+const fs = require ('fs');
+const notesTaker = require ('../db/db.json')
 
 
 // ===============================================================================
@@ -19,14 +20,18 @@ module.exports = function(app) {
   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
 
-  app.get("/api/tables", function(req, res) {
-    res.json(tableData);
+  app.get("/api/notes", function(req, res) {
+    console.log("Note success")
+    res.json(notesTaker);
   });
 
-  app.get("/api/waitlist", function(req, res) {
-    res.json(waitListData);
-  });
+  app.get ("/api/notes/:id", function (req, res) {
+  //  JSON.parse to read db.json
+  let saveNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  res.json(tableData);
 
+  })
+ 
   // API POST Requests
   // Below code handles when a user submits a form and thus submits data to the server.
   // In each of the below cases, when a user submits form data (a JSON object)
@@ -34,19 +39,15 @@ module.exports = function(app) {
   // (ex. User fills out a reservation request... this data is then sent to the server...
   // Then the server saves the data to the tableData array)
   // ---------------------------------------------------------------------------
+  app.post("/api/notes", function(req, res) {
+    console.log()
+    let newNoteTaker = req.body;
+    newNoteTaker.id
+    res.json(notesTaker);
+  
+    // Note the code here. Our "server" will respond to requests and let users know if they have a note or not.
 
-  app.post("/api/tables", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body parsing middleware
-    if (tableData.length < 5) {
-      tableData.push(req.body);
-      res.json(true);
-    }
-    else {
-      waitListData.push(req.body);
-      res.json(false);
-    }
+   
   });
 
   // ---------------------------------------------------------------------------
@@ -60,4 +61,15 @@ module.exports = function(app) {
 
     res.json({ ok: true });
   });
+
+
+  app.delete("/api/notes/:id", function (req, res) {
+    // converting id to string
+    // ============================
+
+    let id = req.params.id.toString();
+    console.log(id);
+
+    
+  }
 };
