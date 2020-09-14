c0da4175e6fda1f2e13167c22423b92a88c93ef8
Elham Fabe <elhamfabe@gmail.com>
2020-09-14 01:14:59 -0400

    read me

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # Note-Taker
+
+  [![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)
+
+  Contents of This File
+  * [Description](#description)
+  * [Technologies](#technologies)
+  * [Installation](#installation)
+  * [Contributors](#contributors)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  * [License](#license)
+
+  # Description 
+    Note taker application allows users to add, store and delete notes with ease. 
+
+  ### Technologies:
+
+    * HTML
+    * CSS
+    * Api Routes
+    * Javascript
+    * gitignore
+    * Express
+
+ ---
+
+  ### Installation:
+
+  run on terminal (MacOS) or git bash (windows) npm install (npm i)
+
+  #### Usage :
+
+    * Clone repository  
+    * Install Node.js
+
+
+ #### License:
+  [![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)
+  ---
+
+  #### Tests:
+  NaN
+
+  ### Contributors:
+  Elham T.Hussain : https://github.com/ElhamFabe/Note-Taker
+
+  #### Questions:
+  Elhamfabe@gmail.com
+ ---
+
+  
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -15,9 +15,6 @@ const notesTaker = require ('../db/db.json')
 
 module.exports = function(app) {
   // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
 
   app.get("/api/notes", function(req, res) {
@@ -28,21 +25,15 @@ module.exports = function(app) {
   app.get ("/api/notes/:id", function (req, res) {
   //  JSON.parse to read db.json
   let saveNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-  res.json(tableData);
+  res.json(saveNotes);
 
   })
  
   // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
   // ---------------------------------------------------------------------------
   app.post("/api/notes", function(req, res) {
     console.log()
-    let newNoteTaker = req.body;
-    newNoteTaker.id
+  notesTaker.push(req, body);
     res.json(notesTaker);
   
     // Note the code here. Our "server" will respond to requests and let users know if they have a note or not.
@@ -50,19 +41,7 @@ module.exports = function(app) {
    
   });
 
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
-
-  app.post("/api/clear", function(req, res) {
-    // Empty out the arrays of data
-    tableData.length = 0;
-    waitListData.length = 0;
-
-    res.json({ ok: true });
-  });
-
-
+  // API DELETE Requests
   app.delete("/api/notes/:id", function (req, res) {
     // converting id to string
     // ============================
@@ -71,5 +50,5 @@ module.exports = function(app) {
     console.log(id);
 
     
-  }
+  });
 };
