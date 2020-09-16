7182700360032968a4d998a98565e710add585f0
Elham Fabe <elhamfabe@gmail.com>
2020-09-16 15:56:26 -0400

    working post and delete

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,41 +4,58 @@
 
 
 const express = require('express');
-const path = require ('path');
-const fs = require ('fs');
-const notesTaker = require ('../db/db.json')
+const path = require('path');
+const fs = require('fs');
+const notesTaker = require('../db/db.json')
 
 
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
-module.exports = function(app) {
+module.exports = function (app) {
   // API GET Requests
   // ---------------------------------------------------------------------------
 
-  app.get("/api/notes", function(req, res) {
+  app.get("/api/notes", function (req, res) {
     console.log("Note success")
     res.json(notesTaker);
   });
 
-  app.get ("/api/notes/:id", function (req, res) {
-  //  JSON.parse to read db.json
-  let saveNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-  res.json(saveNotes);
 
-  })
- 
+
   // API POST Requests
   // ---------------------------------------------------------------------------
-  app.post("/api/notes", function(req, res) {
-    console.log()
-  notesTaker.push(req, body);
-    res.json(notesTaker);
-  
+  app.post("/api/notes", function (req, res) {
+    console.log(req.body)
+    const newNote = req.body
+    const lastIndex = notesTaker.length - 1
+    if (lastIndex < 0) {
+      newNote.id = 1
+    } else {
+      const lastId = notesTaker[lastIndex].id
+      newNote.id = lastId + 1
+    }
+    notesTaker.push(newNote);
+    fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(notesTaker), function (err) {
+      if (err) {
+        console.log(err)
+      }
+
+      console.log('write file')
+      fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", function (err, data) {
+        if (err) {
+          console.log(err)
+        }
+        console.log("read data")
+        console.log(data)
+        res.json(data);
+      })
+    })
+
     // Note the code here. Our "server" will respond to requests and let users know if they have a note or not.
 
-   
+
   });
 
   // API DELETE Requests
@@ -46,9 +63,26 @@ module.exports = function(app) {
     // converting id to string
     // ============================
 
-    let id = req.params.id.toString();
+    const id = parseInt(req.params.id)
     console.log(id);
+    const findIndex = notesTaker.findIndex(el => el.id === id)
 
-    
+    notesTaker.splice(findIndex, 1)
+
+    fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(notesTaker), function (err) {
+      if (err) {
+        console.log(err)
+      }
+
+      console.log('write file')
+      fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", function (err, data) {
+        if (err) {
+          console.log(err)
+        }
+        console.log("read data")
+        console.log(data)
+        res.json(data);
+      })
+    });
   });
-};
+}
